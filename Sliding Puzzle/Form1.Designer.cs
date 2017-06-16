3a8ebbc816e3195022022e5f53b30e3c03bf1e56
achil <achil@TURBOX-LAPTOP>
2017-06-16 22:01:15 +0300

    Add project files.

---
+++ Sliding Puzzle/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Sliding_Puzzle
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic00 = new System.Windows.Forms.PictureBox();
+            this.pnlBackboard = new System.Windows.Forms.Panel();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnRestartGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.timerUpdateLabel = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic00)).BeginInit();
+            this.pnlBackboard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pic00
+            // 
+            this.pic00.BackColor = System.Drawing.Color.Red;
+            this.pic00.Location = new System.Drawing.Point(33, 80);
+            this.pic00.Name = "pic00";
+            this.pic00.Size = new System.Drawing.Size(57, 87);
+            this.pic00.TabIndex = 0;
+            this.pic00.TabStop = false;
+            this.pic00.Visible = false;
+            // 
+            // pnlBackboard
+            // 
+            this.pnlBackboard.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlBackboard.Controls.Add(this.pic00);
+            this.pnlBackboard.Location = new System.Drawing.Point(147, 50);
+            this.pnlBackboard.Name = "pnlBackboard";
+            this.pnlBackboard.Size = new System.Drawing.Size(440, 440);
+            this.pnlBackboard.TabIndex = 0;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btnNewGame.Location = new System.Drawing.Point(613, 440);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(114, 50);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnRestartGame
+            // 
+            this.btnRestartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btnRestartGame.Location = new System.Drawing.Point(12, 440);
+            this.btnRestartGame.Name = "btnRestartGame";
+            this.btnRestartGame.Size = new System.Drawing.Size(114, 50);
+            this.btnRestartGame.TabIndex = 1;
+            this.btnRestartGame.Text = "Restart";
+            this.btnRestartGame.UseVisualStyleBackColor = true;
+            this.btnRestartGame.Click += new System.EventHandler(this.btnRestartGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(278, 516);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time";
+            this.label1.Visible = false;
+            // 
+            // txtTime
+            // 
+            this.txtTime.BackColor = System.Drawing.SystemColors.Window;
+            this.txtTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.txtTime.Location = new System.Drawing.Point(325, 513);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.ReadOnly = true;
+            this.txtTime.Size = new System.Drawing.Size(100, 26);
+            this.txtTime.TabIndex = 3;
+            this.txtTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timerUpdateLabel
+            // 
+            this.timerUpdateLabel.Interval = 1000;
+            this.timerUpdateLabel.Tick += new System.EventHandler(this.timerUpdateLabel_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(739, 567);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRestartGame);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.pnlBackboard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Sliding Puzzle";
+            ((System.ComponentModel.ISupportInitialize)(this.pic00)).EndInit();
+            this.pnlBackboard.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic00;
+        private System.Windows.Forms.Panel pnlBackboard;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnRestartGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timerUpdateLabel;
+        private System.Windows.Forms.TextBox txtTime;
+    }
+}
+
---
+++ Sliding Puzzle/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sliding_Puzzle
+{
+    public partial class frmMain : Form
+    {
+        // I think I've got wrong the i, j positions...
+        static byte backboardSize = 2;
+        static byte pixels = 2;
+        static byte tileWidth;
+        static System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
+        // Create the pictureBoxes:
+        static Tile[,] tiles = new Tile[backboardSize, backboardSize];
+        // With the `tilesSimpleArray` I just want to create a simple List that will have the
+        // references pointing to the original `Tile tiles` line above.
+        static List<Tile> tilesSimpleList = new List<Tile>();
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            
+            createGame(gameState.InitialGame);
+        }
+
+        public string asd()
+        {
+            return txtTime.Text;
+        }
+
+        enum gameState
+        {
+            InitialGame,
+            NewGame,
+            Restart,
+            End
+        }
+
+        private void manageTimer(gameState state)
+        {
+            switch (state)
+            {
+                case gameState.InitialGame:
+                    stopwatch.Start();
+                    timerUpdateLabel.Start();
+                    break;
+                case gameState.NewGame:
+                    txtTime.Text = "00:00:00"; // In order to stop the delay.
+                    timerUpdateLabel.Stop(); // In order to stop the delay.
+                    timerUpdateLabel.Start(); // In order to stop the delay.
+                    stopwatch.Restart();
+                    break;
+                case gameState.Restart:
+                    break;
+                case gameState.End:
+                    timerUpdateLabel.Stop();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void createGame(gameState state)
+        {
+            tilesSimpleList.Clear();
+            manageTimer(state);
+            
+            tileWidth = (byte)((pnlBackboard.Width - pixels * (backboardSize + 1)) / backboardSize); ;
+
+            for (byte i = 0; i < backboardSize; i++) // Rows:
+            {
+                for (byte j = 0; j < backboardSize; j++) // Columns:
+                {
+                    if (state == gameState.NewGame || state == gameState.Restart)
+                    {
+                        pnlBackboard.Controls.Remove(tiles[i, j].GetPicture());
+                    }
+                    int locationLeft = pixels + i * (pixels + tileWidth);
+                    int locationTop = pixels + j * (pixels + tileWidth);
+                    tiles[i, j] = new Tile(locationLeft, locationTop, i, j, this);
+                    tilesSimpleList.Add(tiles[i, j]);
+                    pnlBackboard.Controls.Add(tiles[i, j].GetPicture());
+                }
+            }
+            setNeighbours(tiles); // I don't like this line here
+        }
+
+        private static bool checkOutOfBoundaries(int x, int y, int max)
+        {
+            if (x > -1 && x < max && y > -1 && y < max)
+                return true;
+            else
+                return false;
+        }
+
+        private static void setNeighbours(Tile[,] in_tiles)
+        {
+            for (byte i = 0; i < backboardSize; i++) // Rows:
+            {
+                for (byte j = 0; j < backboardSize; j++) // Columns:
+                {
+                    in_tiles[i, j].Neighbour = false;
+                    try
+                    {
+                        // North neighbour:
+                        if (checkOutOfBoundaries(Tile.hidden.X - 1, Tile.hidden.Y, backboardSize))
+                        {
+                            in_tiles[Tile.hidden.X - 1, Tile.hidden.Y].Neighbour = true;
+                        }
+                        // West neightbour:
+                        if (checkOutOfBoundaries(Tile.hidden.X, Tile.hidden.Y - 1, backboardSize))
+                        {
+                            in_tiles[Tile.hidden.X, Tile.hidden.Y - 1].Neighbour = true;
+                        }
+                        // East neightbour:
+                        if (checkOutOfBoundaries(Tile.hidden.X, Tile.hidden.Y + 1, backboardSize))
+                        {
+                            in_tiles[Tile.hidden.X, Tile.hidden.Y + 1].Neighbour = true;
+                        }
+                        // South neightbour:
+                        if (checkOutOfBoundaries(Tile.hidden.X + 1, Tile.hidden.Y, backboardSize))
+                        {
+                            in_tiles[Tile.hidden.X + 1, Tile.hidden.Y].Neighbour = true;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private class Tile : ITile
+        {
+            // Static variables:
+            public static Point hidden; // Which tile is the hidden one?
+            private static List<int> shuffledList = Enumerable.Range(0, frmMain.backboardSize * frmMain.backboardSize).Shuffle().ToList();
+            private static int count = 0;
+
+            private frmMain frm;
+            private PictureBox pic = new PictureBox();
+            private Point gridCoords; // i, j. - ie. 2,3
+            private bool isNeighbour = false; // Which tiles are the 4-neighbours?
+            
+            public static void ResetStaticVariables(gameState state)
+            {
+                if (state == gameState.NewGame)
+                {
+                    shuffledList = Enumerable.Range(0, frmMain.backboardSize * frmMain.backboardSize).Shuffle().ToList();
+                }
+                else if (state == gameState.Restart)
+                {
+                    // Just keep the shuffledList untouched.
+                }
+                count = 0;
+            }
+
+            public bool Neighbour
+            {
+                set
+                {
+                    this.isNeighbour = value;
+                }
+                get
+                {
+                    return this.isNeighbour;
+                }
+            }
+
+            public void Initialize()
+            {
+                pic.Tag = shuffledList[count].ToString();
+                count++;
+
+                pic.BackColor = Color.DarkGreen;
+                pic.Size = new Size(frmMain.tileWidth, frmMain.tileWidth);
+                // The PaintEventHandler is an event that's happening continuously.
+                // so, when the "Tag" is changed, the drawing changes as well!
+                pic.Paint += new PaintEventHandler((s, e) => pic_Paint(s, e, (string)pic.Tag));
+                pic.Click += new EventHandler(swap);
+
+                if (pic.Tag.ToString() == "0")
+                {
+                    setHidden();
+                }
+            }
+
+            private void setHidden()
+            {
+                hidden = new Point(gridCoords.X, gridCoords.Y);
+                // Can I do hidden = gridCoords as value and not as reference?
+
+                pic.Visible = false;
+            }
+
+            private void swap(object sender, EventArgs e)
+            {
+                if (isNeighbour) // The swap works only in the 4-neighbourhood
+                {
+                    // Can I do a simple swap of the objects instead of changing all of their properties?
+                    tiles[hidden.X, hidden.Y].GetPicture().Visible = true; // Unhide
+                    tiles[hidden.X, hidden.Y].GetPicture().Tag = pic.Tag;
+                    pic.Tag = "0";
+                    setHidden();
+                    setNeighbours(tiles);
+
+                    // Check if the puzzle is solved:
+
+                    var currentSequence = new List<int>();
+                    for (int i = 0; i < backboardSize; i++)
+                    {
+                        for (int j = 0; j < backboardSize; j++)
+                        {
+                            currentSequence.Add(Convert.ToInt32(tiles[j, i].GetPicture().Tag)); // failed here with i, j
+                        }
+                    }
+
+                    if (currentSequence[0] == 1) // Must begin with '1'
+                    {
+                        bool won = true;
+                        for (int i = 1; i < currentSequence.Count - 1; i++)
+                        {
+                            if (currentSequence[i] - currentSequence[i - 1] != 1) // Must be a sequence
+                            {
+                                won = false;
+                                break;
+                            }
+                        }
+                        if (won)
+                        {
+                            foreach (Tile tile in tilesSimpleList)
+                            {
+                                tile.GetPicture().Click -= new EventHandler(swap);
+                            }
+                            
+                            /*
+                            for (int i = 0; i < backboardSize; i++)
+                            {
+                                for (int j = 0; j < backboardSize; j++)
+                                {
+                                    tiles[i, j].GetPicture().Click -= swap; // failed here with i, j
+                                }
+                            }
+                            */
+
+                            frm.manageTimer(gameState.End);
+                            string text = "You have won the match!\n\nTime: " + frm.txtTime.Text;
+                            MessageBox.Show(text, "Congratulations!");
+                        }
+                    }
+                }
+            }
+
+            private void pic_Paint(object sender, PaintEventArgs e, string text)
+            {
+                using (Font myFont = new Font("Arial", 14, FontStyle.Bold))
+                {
+                    // Create a StringFormat object with the each line of text, and the block
+                    // of text centered on the page.
+                    StringFormat stringFormat = new StringFormat();
+                    stringFormat.Alignment = StringAlignment.Center;
+                    stringFormat.LineAlignment = StringAlignment.Center;
+                    e.Graphics.DrawString(text, myFont, Brushes.White, new Point(frmMain.tileWidth / 2, frmMain.tileWidth / 2), stringFormat);
+                }
+            }
+
+            public PictureBox GetPicture()
+            {
+                return pic;
+            }
+
+            public Tile(int x, int y, int in_i, int in_j, frmMain frm)
+            {
+                this.frm = frm;
+                pic.Location = new Point(x, y);
+                this.gridCoords = new Point(in_i, in_j);
+
+                Initialize();
+            }
+
+            //public Tile(byte x, byte y, byte in_i, byte in_j) :
+            //    this((int)x, y, in_i, in_j)
+            //{
+            //
+            //}
+        }
+
+        interface ITile
+        {
+            void Initialize();
+            PictureBox GetPicture();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            Tile.ResetStaticVariables(gameState.NewGame);
+            createGame(gameState.NewGame);
+        }
+
+        private void btnRestartGame_Click(object sender, EventArgs e)
+        {
+            Tile.ResetStaticVariables(gameState.Restart);
+            createGame(gameState.Restart);
+        }
+
+        private void timerUpdateLabel_Tick(object sender, EventArgs e)
+        {
+            // Update every second:
+            //string labelText = stopwatch.Elapsed.Hours + ":" + stopwatch.Elapsed.Minutes
+            //    + ":" + stopwatch.Elapsed.Seconds;
+
+            double seconds = Math.Round(stopwatch.ElapsedMilliseconds / 1000.0);
+            double minutes = Math.Floor(seconds / 60);
+            seconds %= 60;
+            double hours = Math.Floor(minutes / 60);
+            minutes %= 60;
+
+            string text = (hours < 10) ? "0" + hours.ToString() : hours.ToString();
+            text += (minutes < 10) ? ":0" + minutes.ToString() : ":" + minutes.ToString();
+            text += (seconds < 10) ? ":0" + seconds.ToString() : ":" + seconds.ToString();
+            txtTime.Text = text;
+        }
+    }
+}
